88428a269a7f8e3d4178fe0a0e9973c919abced7
rahmed19 <ray.ahmed.sec@gmail.com>
2021-04-26 15:25:28 -0700

    Product image adjustment

--- src/components/Products/Product/product.jsx
+++ src/components/Products/Product/product.jsx
@@ -11,7 +11,7 @@ export default function Product({ product }) {
 
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image='' title={product.name} />
+            <CardMedia className={classes.media} image={product.image} title={product.name} />
             <CardContent>
                 <div className={classes.cardContent}>
                     <Typography variant="h5" gutterBottom>
@@ -21,7 +21,7 @@ export default function Product({ product }) {
                         {product.price}
                     </Typography>
                 </div>
-                <Typography variant="h2" color="textSecondary">{product.description}</Typography>
+                <Typography variant="body2" color="textSecondary">{product.description}</Typography>
             </CardContent>
             <CardActions disabledSpacing className={classes.cardActions}>
                 <IconButton aria-label="Add to Card">
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,8 +4,8 @@ import { Grid } from '@material-ui/core'
 import Product from './Product/Product'
 
 const products = [
-    { id: 1, name: 'Shoes', description: 'Running Shoes.', price: '$5.00' },
-    { id: 2, name: 'Macbook', description: 'Apple Macbook Crap', price: '$10.00' }
+    { id: 1, name: 'Shoes', description: 'Running Shoes.', price: '$5.00', image: 'https://images.pexels.com/photos/1599005/pexels-photo-1599005.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' },
+    { id: 2, name: 'Macbook', description: 'Apple Macbook Crap', price: '$10.00', image: 'https://images.pexels.com/photos/4186329/pexels-photo-4186329.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' }
 ]
 
 export default function Products() {
