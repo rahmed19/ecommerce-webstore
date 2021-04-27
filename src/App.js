3a6ce5dfca14e35a7df8cb698d8885db97fa5e06
rahmed19 <ray.ahmed.sec@gmail.com>
2021-04-27 03:02:26 -0700

    Navbar setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import Products from './components/Products/Products'
+// import Navbar from './components/Navbar/Navbar'
+// import Products from './components/Products/Products'
+
+import { Navbar, Products } from './components'
 
 export default function () {
     return (
         <div>
+            <Navbar />
             <Products />
         </div>
     )
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,7 +5,7 @@ import Product from './Product/Product'
 
 const products = [
     { id: 1, name: 'Shoes', description: 'Running Shoes.', price: '$5.00', image: 'https://images.pexels.com/photos/1599005/pexels-photo-1599005.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' },
-    { id: 2, name: 'Macbook', description: 'Apple Macbook Crap', price: '$10.00', image: 'https://images.pexels.com/photos/4186329/pexels-photo-4186329.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' }
+    { id: 2, name: 'Macbook', description: 'Apple Macbook Crap', price: '$10.00', image: 'https://images.pexels.com/photos/4930607/pexels-photo-4930607.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' }
 ]
 
 export default function Products() {
