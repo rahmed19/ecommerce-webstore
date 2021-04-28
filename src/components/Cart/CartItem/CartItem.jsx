c4dcb6ff5ad110b52d2cd172d13d7d30c08e8e96
rahmed19 <ray.ahmed.sec@gmail.com>
2021-04-28 03:10:31 -0700

    CartItem complete

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 import { Typography, Card, Button, CardActions, CardContent, CardMedia } from '@material-ui/core'
 import useStyles from './styles'
 
-export default function CartItem() {
+export default function CartItem({ item }) {
+    const classes = useStyles()
     return (
-        <div>
-
-        </div>
+        <Card>
+            <CardMedia image={item.media.source} alt={item.name} className={classes.media} />
+            <CardContent className={classes.cardContent}>
+                <Typography variant="h4">{item.name}</Typography>
+                <Typography variant="h5">{item.line_total.formatted_with_symbol}</Typography>
+            </CardContent>
+            <CardActions className={classes.classActions}>
+                <div className={classes.buttons}>
+                    <Button type="button" size="small">-</Button>
+                    <Typography>{item.quantity}</Typography>
+                    <Button type="button" size="small">+</Button>
+                </div>
+                <Button variant="contained" type="button" color="secondary">Remove</Button>
+            </CardActions>
+        </Card>
     )
 }
